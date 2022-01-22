7fb63d1cc59ffbf63c688ae4d977bb4f856e44d7
Rodrigo de França <howdrigo.fr@gmail.com>
2022-01-21 22:42:07 -0300

    feat: add desafio cobras e escadas

--- run.py
+++ run.py
@@ -2,24 +2,40 @@ from os import system
 
 def rodar_desafio():
   print("""
-  O primeiro teste foi implementado em duas linguagens:
-  1. Dart
-  2. Javascript
+  Qual teste deseja rodar?
+  1 - Teste 1
+  2 - Teste 2
   """)
 
-  desafio = input('Qual linguagem você quer rodar: ').lower()
+  opcao = input("Digite a opção desejada: ")
 
-  while desafio not in ["1", "2", "dart", "javascript"]:
-    print("Valor inválido, experimente usar o numero da opção ou o nome da linguagem")
-    desafio = input('Qual linguagem você quer rodar: ')
+  while opcao not in ["1", "2"]:
+    print("Opção inválida!")
+    opcao = input("Digite a opção desejada: ")
   
-  if desafio in ["1", "dart"]:
-    print("\nExecutando desafio 1 na linguagem Dart\n")
-    system("dart teste-01/main.dart")
+  if opcao == "1":
+    print("""
+    O primeiro teste foi implementado em duas linguagens:
+    1 - Dart
+    2 - Javascript
+    """)
 
-  if desafio in ["2", "javascript"]:
-    print("\nExecutando desafio 1 na linguagem Javascript\n")
-    system("node teste-01/main.js")
+    desafio = input('Qual linguagem você quer rodar: ').lower()
+
+    while desafio not in ["1", "2", "dart", "javascript"]:
+      print("Valor inválido, experimente usar o numero da opção ou o nome da linguagem")
+      desafio = input('Qual linguagem você quer rodar: ')
+    
+    if desafio in ["1", "dart"]:
+      print("\nExecutando desafio 1 na linguagem Dart\n")
+      system("dart teste_01/main.dart")
+
+    if desafio in ["2", "javascript"]:
+      print("\nExecutando desafio 1 na linguagem Javascript\n")
+      system("node teste_01/main.js")
+  else:
+    print("\nExecutando desafio 2 (Cobras e Escadas)\n")
+    system("cd teste_02 && flutter run || cd ..")
 
 
 rodar_desafio()
